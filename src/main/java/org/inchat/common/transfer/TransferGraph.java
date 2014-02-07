54a587e78325e7b8f488a6b27f2ac8a1a8ff9f28
Rene <rene.bernhardsgruetter@gmail.com>
2014-02-07 07:06:38 +0100

    an unfinished outine of the transfer graph parts. this need a lot of work. this commit is just to share it with others (and to my notebook...).

---
+++ src/main/java/org/inchat/common/transfer/TransferGraph.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2013, 2014 inchat.org
+ *
+ * This file is part of inchat-common.
+ *
+ * inchat-common is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * inchat-common is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.inchat.common.transfer;
+
+import org.inchat.common.Message;
+
+public class TransferGraph {
+
+    Vertex root;
+
+    public TransferGraph(Message root) {
+        if (root == null) {
+            throw new IllegalArgumentException("The argument may not be null.");
+        }
+
+        this.root = new Vertex(root);
+    }
+
+}
---
+++ src/test/java/org/inchat/common/transfer/TransferGraphTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2013, 2014 inchat.org
+ *
+ * This file is part of inchat-common.
+ *
+ * inchat-common is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * inchat-common is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.inchat.common.transfer;
+
+import org.inchat.common.Message;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+public class TransferGraphTest {
+
+    private TransferGraph graph;
+    private Message plaintext;
+    private byte[] ciphertext;
+
+    @Before
+    public void setUp() {
+        plaintext = new Message();
+        ciphertext = new byte[0];
+
+        graph = new TransferGraph(plaintext);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorOnNull() {
+        graph = new TransferGraph(null);
+    }
+
+    @Test
+    public void testConstructorOnAssignment() {
+        graph = new TransferGraph(plaintext);
+        assertSame(plaintext, graph.root.plaintext);
+    }
+
+}
